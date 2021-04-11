52406b5e4d077c7e515cded2e7462c419bff738f
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2021-04-11 12:02:11 +0900

    add: BookCard

--- web/src/pages/userProfile.tsx
+++ web/src/pages/userProfile.tsx
@@ -2,26 +2,12 @@ import withApollo from '../utils/withApollo';
 import {
   useMyPublishBooksQuery,
   useMySubscribeBooksQuery,
-  useReturnBookMutation,
 } from '../generated/graphql';
 import { Header } from '../components/Header';
-import {
-  Text,
-  Button,
-  Link,
-  Image,
-  Box,
-  Tabs,
-  TabList,
-  TabPanels,
-  Tab,
-  TabPanel,
-} from '@chakra-ui/react';
-import { omitString } from '../utils/omitString';
-import { useRouter } from 'next/router';
+import { Box, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import { BookCard } from '../components/BookCard';
 
 const UserProfile: React.FC = () => {
-  const router = useRouter();
   const {
     data: subData,
     loading: subLoading,
@@ -33,8 +19,6 @@ const UserProfile: React.FC = () => {
     loading: pubLoading,
     error: pubError,
   } = useMyPublishBooksQuery();
-
-  const [returnBook] = useReturnBookMutation();
 
   if (subError || pubError) return <p>Error :(</p>;
   if (subLoading || pubLoading) return <p>Loading...</p>;
@@ -61,72 +45,17 @@ const UserProfile: React.FC = () => {
           <TabPanels>
             <TabPanel>
               {subData.mySubscribeBooks?.map((book) => (
-                <Box
+                <BookCard
                   key={book.id}
-                  p={2}
-                  mb={4}
-                  display="flex"
-                  borderRadius="sm"
-                  borderWidth="thin"
-                  borderColor="gray.200"
-                  position="relative"
-                >
-                  <Box flexShrink={0}>
-                    <Image
-                      width={{ md: 48 }}
-                      objectFit="cover"
-                      borderRadius="lg"
-                      src={book.img}
-                      alt="Book image"
-                    />
-                  </Box>
-                  <Box mt={{ base: 2, md: 0 }} ml={{ base: 4, md: 6 }}>
-                    <Text
-                      mt={1}
-                      display="block"
-                      fontSize="xl"
-                      lineHeight="normal"
-                      fontWeight="semibold"
-                    >
-                      {book.title}
-                    </Text>
-                    <Text
-                      display={{ base: 'none', md: 'block' }}
-                      mt={2}
-                      color="gray.500"
-                      fontSize="md"
-                    >
-                      {omitString(book.description)}
-                    </Text>
-                    <Button
-                      position="absolute"
-                      right="4"
-                      bottom="4"
-                      variant="outline"
-                      colorScheme="teal"
-                      onClick={async () => {
-                        try {
-                          const res = await returnBook({
-                            variables: { id: book.id },
-                          });
-                          if (!res.data?.returnBook) {
-                            alert('return book faild');
-                          } else {
-                            router.push('/');
-                          }
-                        } catch (err) {
-                          alert(err);
-                        }
-                      }}
-                    >
-                      本を返す
-                    </Button>
-                  </Box>
-                </Box>
+                  book={book}
+                  buttonVariant="returnBook"
+                />
               ))}
             </TabPanel>
             <TabPanel>
-              <p>two!</p>
+              {pubData.myPublishBooks?.map((book) => (
+                <BookCard key={book.id} book={book} />
+              ))}
             </TabPanel>
           </TabPanels>
         </Tabs>
